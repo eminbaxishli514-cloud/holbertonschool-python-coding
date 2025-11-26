24bd6162d04865cdfba166a0ff6b6d344c12326d
eminbaxishli514-cloud <eminbaxishli2@gmail.com>
2025-11-26 12:04:23 +0400

    My new commit

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -2,5 +2,8 @@
 
 
 class Square:
+    """A class that defines a square by its size."""
+
     def __init__(self, size):
+        """Initialize the square with a private size attribute."""
         self.__size = size
